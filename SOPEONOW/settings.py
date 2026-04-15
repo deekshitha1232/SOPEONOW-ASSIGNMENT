e03c34c98d808586eaa3ef22e5188d95621de376
deekshitha1232 <deekshamedi45@gmail.com>
2026-04-15 12:56:06 +0530

    Update settings.py

--- SOPEONOW/settings.py
+++ SOPEONOW/settings.py
@@ -49,6 +49,7 @@
 #     'django.contrib.auth.middleware.AuthenticationMiddleware',
 #     'django.contrib.messages.middleware.MessageMiddleware',
 #     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+      
 # ]
 
 # ROOT_URLCONF = 'SOPEONOW.urls'
@@ -158,6 +159,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'SOPEONOW.urls'
